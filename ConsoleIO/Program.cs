46ccb0ddf7553c46eee964ba06a3f8c8888be123
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-10 11:07:05 +0530

    Added ifElse

---
+++ ConsoleIO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow my name is Siddharth");
+
+            Console.ReadLine();
+            string name;
+            name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ IfStatemenets/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfStatemenets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int age = 22;
+            string name = "John";
+
+            if (age == 22)
+            {
+                Console.WriteLine("wow you are 22");
+            }
+            //with int you can use >=, <=, ==, !=, >, <
+            //with string you can use ==, !=
+            if(name == "John" || age>=22)
+            {
+                Console.WriteLine("wow your name is John and so big");
+            }
+            else if (name == "John" && age <22)
+            {
+                Console.WriteLine("you are not John or something");
+            }
+            else
+            {
+                Console.WriteLine("you are not John");
+            }
+
+            string xInput = Console.ReadLine();
+            int x = Convert.ToInt32(xInput);
+
+            string yInput = Console.ReadLine();
+            int y = Convert.ToInt32(yInput);
+
+            string ansInput = Console.ReadLine();
+            int ans = Convert.ToInt32(ansInput);
+
+            if (x + y == ans)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Incorrect");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
