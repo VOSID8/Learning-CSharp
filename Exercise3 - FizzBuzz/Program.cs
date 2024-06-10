df28a1856390a41766a40dcee012f2d37e28e182
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-10 12:40:44 +0530

    Exercise FizzBuzz

---
+++ Exercise3 - FizzBuzz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3___FizzBuzz
+{
+    internal class Program
+    {
+
+        /*
+         * Create a for loop from 1 to X (15)
+         * 3 and 5 =  FizzBuzz
+         * 3 = Fizz
+         * 5 = Buzz
+         * else = number
+        */
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
