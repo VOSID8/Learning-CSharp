25dcc53645e8d54d0e29b2d056c80d1bec6415c0
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-10 12:13:10 +0530

    Added loops

---
+++ ForLoops/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Write("How many times do you want to loop? ");
+            int loop = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < loop; i++)
+            {
+                Console.WriteLine("HII");
+            }
+
+            Console.WriteLine("HERE");
+
+            for(int i = 0; i < 10; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            string count = Console.ReadLine();
+
+            int j = 0;
+            while (j < int.Parse(count))
+            {
+                Console.WriteLine("YOOO");
+                j++;
+            }
+
+            //condition ? true : false
+            int k = 0;
+            while (k < 10)
+            {
+                Console.WriteLine(k % 2 == 0 ? "Even" : "Odd");
+                k++;
+            }  
+
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey(); 
+            
+            
+        }
+    }
+}
