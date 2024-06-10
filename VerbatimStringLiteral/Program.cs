a8e587a197ddea6fbf02d5c9baa9f40325f46ba5
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-10 12:49:33 +0530

    Added string literals

---
+++ VerbatimStringLiteral/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerbatimStringLiteral
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //\t \n"
+            string path = "C:\\Program Files\\Microsoft Visual Studio 10.0";
+            Console.WriteLine(path);
+            string speech = "\"Hello, World!\"";
+
+            path = @"C:\Program Files\Microsoft Visual Studio 10.0";
+
+            Console.WriteLine(path);
+
+            string name = @"Hello ""someone"""; //Hello "someone"
+            string name2 = "Hello \"someone\""; //Hello "someone"
+            string name3 = "Hello \\someone\\"; //Hello \someone\
+            string name4 = "Hello 'someone'"; //Hello 'someone'
+
+            Console.WriteLine(path);
+            Console.ReadLine();
+
+        }
+    }
+}
