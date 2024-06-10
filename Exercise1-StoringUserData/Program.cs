ba02ba7fbccea9929221cc77eb914de75ad84e40
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-10 10:48:36 +0530

    Added 2 exercises

---
+++ Exercise1-StoringUserData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1_StoringUserData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Define a variable to hold your name
+             * Define a variable to hold your phone number
+             * Hold age
+             * Print variables line by line
+             * Define variables using var keyword
+             */
+            string name = "Siddharth";
+            int age = 21;
+            int phoneNumber = 01234567890;
+            //with int, we can't store 0 in the beginning of the number,
+            //so we can use string to store the phone number
+            var age2 = 21
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Phone Number: " + phoneNumber);
+        }
+    }
+}
---
+++ Exercise2-oddEvenChecker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_oddEvenChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2;
+            int remainder = num1 % num2;
+            Console.WriteLine(remainder);
+            num1 = 11;
+            num2 = 2;
+            remainder = num1 % num2;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
