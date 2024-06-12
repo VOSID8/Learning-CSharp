657ec04e0d14c6f60138faa38a3d06c00624db78
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-12 10:09:13 +0530

    Added few modules

---
+++ Exercise - Password Checker/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise___Password_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Enter password again: ");
+            string passwordC = Console.ReadLine();
+
+            if(!password.Equals(string.Empty) && !passwordC.Equals(string.Empty))
+            {
+                if(password.Equals(passwordC))
+                {
+                    Console.WriteLine("Password is correct");
+                }
+                else
+                {
+                    Console.WriteLine("Password is incorrect");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter password");
+            }
+
+        }
+    }
+}
---
+++ StringLooping/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace StringLooping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = "C# is awesome!";
+
+            //char[]
+            Console.WriteLine(message[0]); // C
+            Console.WriteLine(message[1]); // #
+            Console.WriteLine(message[2]); // space
+
+            for(int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine(message.Contains("C#")); // True
+
+
+            bool contains = false;
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == 'C' && message[i + 1] == '#')
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
