bb6e0b670b8442470020298c38e0826d81c76671
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-12 11:44:54 +0530

    Added Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> names = new Dictionary<int, string>
+            {
+                {1, "John"},
+                {2, "Steve"},
+                {3, "Bill" }
+            };
+
+            names.Add(4, "Sam");
+            names.Add(5, "Lucy");
+            names.Add(6, "Test");
+
+            for(int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+            for(int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names.ElementAt(i).Key + " " + names.ElementAt(i).Value);
+            }
+
+            Dictionary<string,string> teachers = new Dictionary<string, string>
+            {
+                {"John", "Math"},
+                {"Steve", "Science"},
+                {"Bill", "English"}
+            };
+
+            if(teachers.TryGetValue("John", out string subject))
+            {
+                Console.WriteLine("John teaches " + subject);
+            }
+            else
+            {
+                Console.WriteLine("John is not a teacher");
+            }
+
+            if (teachers.ContainsKey("Steve"))
+            {
+                teachers.Remove("Steve");
+            }
+            else
+            {
+                Console.WriteLine("Steve is not a teacher");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[5]
+            {
+                2,3,4,5,6
+            };
+            int[] newNumbers = new int[6];
+
+            List<int> listNumbers = new List<int>();
+
+            listNumbers.Add(1);
+            listNumbers.Add(2);
+            listNumbers.Add(3);
+
+            listNumbers.Clear();
+
+            Console.ReadLine();
+        }
+    }
+}
