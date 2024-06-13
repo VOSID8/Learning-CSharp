af166c618c7786f3e5f9fbd334000518fc231c62
Siddharth Banga <91724657+VOSID8@users.noreply.github.com>
2024-06-13 10:13:31 +0530

    Added structures

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int c = a / b;
+
+                Console.Write("Enter a number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter a number between {0} and {1}", int.MinValue, int.MaxValue);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Divide by zero exception");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a valid number");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block");
+            } 
+
+        }
+    }
+}
---
+++ Structures/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    internal class Program
+    {
+
+        struct Person
+        {
+            public string Name;
+            public int Age;
+
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            //string name = "John";
+            //int age = 30;
+
+            Person person1;
+
+            person1.Name = "John";
+            person1.Age = 30;
+
+            ReturnPerson(ref person1.Name, ref person1.Age);
+            Console.WriteLine($"{person1.Name} - {person1.Age}");
+
+            Person person2  = ReturnPerson2();
+
+            Console.WriteLine($"{person2.Name} - {person2.Age}");
+
+            Console.ReadLine();
+        }
+
+        static void ReturnPerson(ref string name, ref int age)
+        {
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        static Person ReturnPerson2() 
+        {
+            Person person = new Person();
+            Console.Write("Enter your name: ");
+            person.Name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            person.Age = Convert.ToInt32(Console.ReadLine());
+
+            return person;
+        }
+    }
+}
